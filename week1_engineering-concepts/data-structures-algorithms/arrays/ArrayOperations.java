2f54be6b885b1f36c97cc0d8fe0cdca75365454b
Ayush Katre <ayushkatre1801@gmail.com>
2025-06-14 08:14:49 +0530

    Completed Array

---
+++ week1_engineering-concepts/data-structures-algorithms/arrays/ArrayOperations.java
@@ -0,0 +1,9 @@
+public class ArrayOperations {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Array elements:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
