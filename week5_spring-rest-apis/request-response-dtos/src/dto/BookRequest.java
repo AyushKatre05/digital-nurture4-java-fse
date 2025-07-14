95fb827c1aebdba78d9e60dabd031a1d0b2a63d0
Ayush Katre <ayushkatre1801@gmail.com>
2025-07-14 13:11:26 +0530

    This module demonstrates clean separation between the domain (`Book`) and API contracts

---
+++ week5_spring-rest-apis/request-response-dtos/src/dto/BookRequest.java
@@ -0,0 +1,13 @@
+package dto;
+
+public class BookRequest {
+    private String title;
+    private String author;
+
+    // Getters & Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+}
